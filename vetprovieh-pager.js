110f046a43557c9863ee384a7c2d75074dcc3aec
Tobias Münch <to.muench@muench-its.de>
2020-05-08 11:36:07 +0200

    Forget to export

--- vetprovieh-pager.js
+++ vetprovieh-pager.js
@@ -1,4 +1,4 @@
-    class VetproviehPager extends HTMLElement {
+    export class VetproviehPager extends HTMLElement {
 
       static get observedAttributes() {
         return ['page', 'maximum'];
